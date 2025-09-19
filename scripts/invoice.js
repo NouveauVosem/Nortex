4ed8aca28866d448696eca0434f53dcc78957058
NouveauVosem <154325333+NouveauVosem@users.noreply.github.com>
2025-09-19 09:19:01 +0200

    thank you - mobile adoptation, products - css change

--- scripts/invoice.js
+++ scripts/invoice.js
@@ -5,55 +5,7 @@ console.log(orderData);
 
 const company = companyTemplate;
 
-// function createInvoice(company, formData) {
-//   return {
-//     // Дані рахунку
-//     invoiceId: "INV-" + formData.orderId,
-//     invoiceDate: formData.dateNow,
 
-//     // Продавець
-//     seller: { ...company },
-
-//     // Покупець (заповнюється з форми)
-//     buyer: {
-//       companyName: formData.companyName,
-//       companyNumber: formData.companyNumber,
-//       firstName: formData.firstName,
-//       lastName: formData.lastName,
-//       email: formData.email,
-//       phone: formData.phone,
-//       address: {
-//         country: formData.country,
-//         city: formData.city,
-//         zip: formData.zip,
-//         companyOrg: formData.companyOrg,
-//         street: formData.address,
-//       },
-//     },
-
-//     // Замовлення
-//     order: {
-//       orderId: formData.orderId,
-//       productId: formData.productId,
-//       productName: formData.productName,
-//       productPrice: Number(formData.productPrice).toFixed(2),
-//       quantity: 1, // якщо замовляється один товар, можна зробити змінним
-//       total: Number(formData.productPrice).toFixed(2),
-//       comment: formData.orderComment,
-//     },
-
-//     // Умови
-//     delivery: formData.delivery,
-//     payerType: formData.payerType,
-//     sameAsDelivery: formData.sameAsDelivery,
-//   };
-// }
-
-// ✅ Приклад використання
-
-// const invoice = createInvoice(companyTemplate, formData);
-
-// console.log("invoice:" + invoice);
 
 function generateInvoice() {
   const priceWithVat = Number(orderData.productPrice); 
